6725c65ffeaf2f75484cfa9eacf0c72b194d5105
Koto3d <69461193+ChupinKr@users.noreply.github.com>
2024-01-17 09:39:56 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Event1XBet event1XBet = new Event1XBet("https://1xlite-917010.top/ru/line/cricket/1852063-big-bash-league/198662853-sydney-thunder-melbourne-renegades");
+    }
+}
---
+++ src/test/java/Test1.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.Configuration.*;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Test1 {
+
+
+    void test1(){
+        open();
+    }
+}
